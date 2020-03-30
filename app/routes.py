be79901eb0020917d8b296c1e30e3193f0e4cf1c
nurbek-b <foto.nurbek@gmail.com>
2020-03-30 22:06:37 +0600

    Login and Logout

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,17 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import request
+from flask import (render_template, flash, redirect, url_for)
+from flask_login import (current_user, login_user, 
+                        logout_user, login_required)
+from werkzeug.urls import url_parse
 from app.forms import LoginForm
-
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Bakyt'}
     posts = [
         {
         'author': {'username': 'Adilet'},
@@ -18,15 +22,28 @@ def index():
         'body': 'Beautiful day in Bishkek, Bbut evry body sitting at home all day'
         }
     ]   
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"""Login requested for user {form.username.data}, 
-        remember_me={form.remember_me.data}"""
-        )
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Ivalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)  
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
